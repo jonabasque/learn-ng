f872e0d570fcb147a5327160a041440cab02c3fd
jonabasque <jonmultimedia@gmail.com>
2014-08-19 18:31:32 +0200

    amadido un metodo al scope del controlador que llamamos al enviar el form

--- js/tareas.js
+++ js/tareas.js
@@ -3,5 +3,12 @@ function ControladorTareas($scope){
         $scope.tareas = [{texto: 'Hacer la compra', realizada: true}, 
                          {texto: 'Lavar la ropa', realizada: false}
                         ];
+        //agregamos al scope del controlador una funcion de agregar tareas
+        $scope.agregarTarea = function() {
+            //al array tarelas le agrega otro objeto. Que recogemos por el scope.
+            $scope.tareas.push({texto: $scope.textoNuevaTarea, realizada:false});
+            //y limpiamos el texto del input una vez agregada la tarea.
+            $scope.textoNuevaTarea = '';
+        }
 
 }
